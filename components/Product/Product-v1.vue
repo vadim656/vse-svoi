e72564eca5738de69eed46603cbea8e9bd974c70
assdim <va.four2016@gmail.com>
2021-12-30 11:46:57 +0300

    fix30.12.21

--- components/Product/Product-v1.vue
+++ components/Product/Product-v1.vue
@@ -1,38 +1,46 @@
 <template>
   <div
-    class="w-[400px] h-[400px] absolute rounded-[15px] overflow-hidden bg-white box-1"
+    class="w-[400px] h-[400px] block rounded-[15px] overflow-hidden bg-white box-1"
   >
     <div class="w-full h-[250px] relative">
       <img
         class="object-cover absolute top-0 left-0 z-[0]"
-        src="../../assets/img/product.jpg"
+        :src="data_img"
         alt=""
       />
       <div class="w-full flex justify-between z-[10] text-white relative">
         <div class="flex w-full mt-4">
-          <div class="w-[50px] h-[50px]  rounded-full flex cc gradient text-white mx-4">
+          <div
+            class="w-[50px] h-[50px]  rounded-full flex cc gradient text-white mx-4"
+          >
             <div class="pl-1">5</div>
-            <img src="../../assets/icons/star.png" alt="">
+            <img src="../../assets/icons/star.png" alt="" />
           </div>
-          <div class="h-[50] rounded-full flex cc gradient text-white px-6">1 200 ₽</div>
+          <div class="h-[50] rounded-full flex cc gradient text-white px-6">
+            {{ data.price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, " ") }} ₽
+          </div>
         </div>
-        <div class="flex flex-col items-end justify-center w-[90px] h-full rounded-tr-[15px] rounded-bl-[15px] px-4 font-14 warn-1 pt-4 pb-2">
-            <span>Сложность</span>
-            <span>4\5</span>
+        <div
+          class="flex flex-col items-end justify-center w-[90px] h-full rounded-tr-[15px] rounded-bl-[15px] px-4 font-14 warn-1 pt-4 pb-2"
+        >
+          <span>Сложность</span>
+          <span>{{ data_complexity }}\5</span>
         </div>
       </div>
     </div>
     <div class="p-4 relative">
       <div class="w-full flex justify-between items-end">
-        <a class="font-20 font-bold cursor-pointer hover:underline">Дом павлова</a>
+        <a class="font-20 font-bold cursor-pointer uppercase hover:underline"
+          >{{ data_name }}</a
+        >
         <span class="font-normal font-14"
-          >Людей: <span class="font-bold font-14">12</span></span
+          >Людей: <span class="font-bold font-14">{{ data_people }}</span></span
         >
       </div>
       <div class="w-full flex justify-between items-end ">
         <ul class="font-18 pt-4">
-          <li>Вид экскурсии: <span class="font-bold">Пешая</span></li>
-          <li>Продолжительность: <span class="font-bold">18 часов</span></li>
+          <li>Вид экскурсии: <span class="font-bold">{{ data_vid }}</span></li>
+          <li>Продолжительность: <span class="font-bold">{{ data_time }}</span></li>
         </ul>
         <AddToCart />
       </div>
@@ -42,7 +50,30 @@
 
 <script>
 import AddToCart from '../UI/AddToCart.vue'
-export default { components: { AddToCart } }
+export default {
+  components: { AddToCart },
+  props: {
+    data: {
+      type: Object,
+      default () {
+        return {}
+      }
+    }
+  },
+  data() {
+    return {
+      data_name: this.data.name,
+      data_vid: this.data.attributes[0].options[0],
+      data_time: this.data.attributes[1].options[0],
+      data_people: this.data.attributes[2].options[0],
+      data_img: this.data.images[0].src,
+      data_complexity: this.data.attributes[3].options[0]
+    }
+  },
+  created() {
+
+  }
+}
 </script>
 
 <style></style>
--- components/TourNear.vue
+++ components/TourNear.vue
@@ -6,24 +6,20 @@
           <span class="text-[48px] uppercase font-bold"
             >Ближайшие экскурсии</span
           >
-          <span class="absolute  left-[50%] text-[288px] text-[#EFEFEF] uppercase font-bold z-[0] select-none leading-none">travel</span>
+          <span
+            class="absolute  left-[50%] text-[288px] text-[#EFEFEF] uppercase font-bold z-[0] select-none leading-none"
+            >travel</span
+          >
           <ShowAllButton class="ml-[50px]  z-[4]">Смотреть все</ShowAllButton>
         </div>
         <div class="w-full h-full  relative block">
-          <div class=" flex justify-between">
-            <div class="block h-[400px]">
-              <ProductV1 />
-            </div>
-            <div class="block h-[400px]">
-              <ProductV1 />
-            </div>
-            <div class="block h-[400px]">
-              <ProductV1 />
-            </div>
-            <div class="block h-[400px]">
-              <ProductV1 />
-            </div>
-
+          <div class=" flex justify-between gap-4">
+            <ProductV1
+              v-for="product in products"
+              :key="product.id"
+              :data="product"
+              class="block h-[400px]"
+            />
           </div>
         </div>
       </div>
@@ -32,11 +28,12 @@
     <div class="container mx-auto h-full  ">
       <div>
         <div class="w-full flex my-12 relative">
-          <span class="text-[48px] uppercase font-bold"
-            >Сезонные туры</span
+          <span class="text-[48px] uppercase font-bold">Сезонные туры</span>
+          <span
+            class="absolute  left-[30%] text-[288px] text-[#EFEFEF] uppercase font-bold z-[0] select-none leading-none"
+            >tourism</span
           >
-          <span class="absolute  left-[30%] text-[288px] text-[#EFEFEF] uppercase font-bold z-[0] select-none leading-none">tourism</span>
-          <ShowAllButton class="ml-[50px] z-[4]" >Смотреть все</ShowAllButton>
+          <ShowAllButton class="ml-[50px] z-[4]">Смотреть все</ShowAllButton>
         </div>
         <div class="w-full h-full  relative block">
           <div class=" flex justify-between">
@@ -52,7 +49,6 @@
             <div class="block h-[560px]">
               <ProductV2 />
             </div>
-
           </div>
         </div>
       </div>
@@ -65,12 +61,37 @@
 </template>
 
 <script>
+import axios from 'axios'
+
 import ShowAllButton from './UI/ShowAllButton.vue'
 import ProductV1 from './Product/Product-v1.vue'
 import ShowAllButton1 from './UI/ShowAllButton.vue'
 
 export default {
-  components: { ShowAllButton, ProductV1, ShowAllButton1 }
+  components: { ShowAllButton, ProductV1, ShowAllButton1 },
+  data () {
+    return {
+      products: []
+    }
+  },
+  created () {
+    axios
+      .get('https://vse-svoi26.ru/wp/wp-json/wc/v3/products/', {
+        auth: {
+          username: 'ck_a1e76b0c4740131ec2a95e3710e1d143bc39758d',
+          password: 'cs_37bfd52d190b6389298d594b76c101c48a4f252e'
+        },
+        params: {
+          per_page: 4
+        }
+      })
+      .then(response => {
+        this.products = response.data
+      })
+      .catch(e => {
+        this.errors.push(e)
+      })
+  }
 }
 </script>
 
